1bd522e2f277735a06edcea48a0f8f3c5e2a9386
Huda Khayrallah <hudak@berkeley.edu>
2018-05-28 13:46:27 -0400

    modified to work with word n-grams

--- local/g2p_selection/g2p/SubmodularObjective.py
+++ local/g2p_selection/g2p/SubmodularObjective.py
@@ -10,7 +10,8 @@ import pdb
 
 class FeatureObjective(object):
     def __init__(self, wordlist, n_order=1, g=np.sqrt, vectorizer='tfidf',
-                 append_ngrams=True, binarize_counts=False):
+                 append_ngrams=True, binarize_counts=False, analyzer='char_wb'):
+        #analyzer is the unit of text we are operating on.  'char_wb' is for characters (eg. G2P) 'word' is for words (eg. MT)
         self.wordlist = wordlist
         self._ngram_vectorizer = None
         self._g = g
@@ -27,6 +28,7 @@ class FeatureObjective(object):
         
         self.get_tfidf_vectorizers()
         self.get_word_features()
+        self.analyzer = analyzer
     
     
     def get_tfidf_vectorizers(self):
@@ -40,7 +42,7 @@ class FeatureObjective(object):
             min_ngram = 1
         ngram_range = (min_ngram, self.n_order)
 
-        vectorizer = self.vectorizer(analyzer="char_wb",
+        vectorizer = self.vectorizer(analyzer=self.analyzer,
                                     encoding="utf-8",
                                     strip_accents=None,
                                     ngram_range=ngram_range)
@@ -87,8 +89,9 @@ class FeatureObjective(object):
         prob_vec = (1.0 + self.subset) / (float(self.subset.sum()) + self.subset.shape[1])
         return np.multiply(self.p, np.log2(self.p + sys.float_info.epsilon) - np.log2(prob_vec)).sum() 
 
-
+    
 class DecayingCrossEntropyObjective(FeatureObjective):
+    ###CUT THIS
     def run(self, idx_new):
         if not idx_new:
             return np.multiply(self.p, np.log2(self.subset + 1)).sum()
@@ -101,6 +104,7 @@ class DecayingCrossEntropyObjective(FeatureObjective):
     
     
 class CrossEntropyObjective(FeatureObjective):
+    #DOES NOT CURRENTLY USE WORDs for MT
     def __init__(self, wordlist, n_order=1, g=np.sqrt, vectorizer='tfidf',
                  append_ngrams=True, prune=False, prune_thresh=0.0000,
                  binarize_counts=False):
@@ -262,220 +266,3 @@ class DSFObjective(FeatureObjective):
         counts = self.W.dot(self._g(counts).T).T
         return np.multiply(np.squeeze(np.asarray(self.total_counts)), self._g(counts)).sum() 
 
-
-#class DSFObjective(FeatureObjective):
-#    def __init__(self, *args, **kwargs):
-#        self._order_idxs = {}
-#        self.W = None
-#        self.total_counts = None
-#        self.K = None
-#        kwargs["append_ngrams"] = True
-#        kwargs["g"] = self.coverage_fun
-#        super(DSFObjective, self).__init__(*args, **kwargs)
-#          
-#
-#    def get_word_features(self):
-#        self.word_features = self._ngram_vectorizer.transform(self.wordlist)
-#       
-#        if self.binarize_counts:
-#            self.word_features[self.word_features > 0 ] = 1.0
-#        self.total_counts = self.word_features.sum(axis=0)
-#        self.p = self.total_counts / float(self.word_features.sum())
-#        self.K = self.total_counts.sum()
-#
-#        for o in range(self.n_order, 0, -1):
-#            self._order_idxs[o] = []
-#            for k, v in self._ngram_vectorizer.vocabulary_.iteritems():
-#                if len(k) == o:
-#                    self._order_idxs[o].append(v)
-#            self._order_idxs[o] = np.asarray(self._order_idxs[o])
-#       
-#        idx_to_ngram = self._ngram_vectorizer.get_feature_names()
-#        row_idx = []
-#        col_idx = []
-#        data = []
-#        
-#        # Sort by ngram order
-#        for o in range(self.n_order, 1, -1):
-#            ograms = [idx_to_ngram[i] for i in self._order_idxs[o]]
-#            for og in ograms:
-#                o_min_1_grams = [i for i in self._order_idxs[o-1] if idx_to_ngram[i] in og]
-#                row_idx.extend(o_min_1_grams)
-#                col_idx.extend(len(o_min_1_grams) * [self._ngram_vectorizer.vocabulary_[og]])
-#                # The 3-gram weight should be a function of the 4-gram freq e.g.
-#                # This way the more common occurrence is weighted more. 
-#                data.extend(len(o_min_1_grams) * [self.total_counts[0, self._ngram_vectorizer.vocabulary_[og]]])
-#                #data.extend(len(o_min_1_grams) * [1.0]) 
-#        
-#        self.W = sparse.csr_matrix((data, (row_idx, col_idx)),
-#            shape=(self.word_features.shape[1], self.word_features.shape[1])) 
-#        
-#        
-#    
-#    def add_to_subset(self, idx):
-#        # Only add max order ngram features in current word
-#        n = min([self.n_order, len(self.wordlist[idx]) + 2])
-#        self.subset[:, self._order_idxs[n]] += self.word_features[idx, self._order_idxs[n]]
-#
-#
-#    def coverage_fun(self, x):
-#        p_vec = np.squeeze(np.asarray(self.total_counts))
-#        vec = np.squeeze(np.asarray(x))
-#        return np.matrix(1.0 - (1.0 / (8.0 ** vec)))
-#
-#
-#    def run(self, idx_new):
-#        vec = self.subset.copy()
-#        n = min([self.n_order, len(self.wordlist[idx_new]) + 2])
-#        vec[:, self._order_idxs[n]] += self.word_features[idx_new, self._order_idxs[n]]
-#
-#        counts = np.zeros(vec.shape)
-#        counts[:, self._order_idxs[self.n_order]] = vec[:, self._order_idxs[self.n_order]]
-#        for n in range(self.n_order, 1, -1):
-#            vec = self._g(vec)
-#            counts = self.W.dot(self._g(counts).T).T
-#            if n >= 2:
-#                counts[:, self._order_idxs[n-1]] += vec[:, self._order_idxs[n-1]]
-#            
-#        
-#        return np.multiply(np.squeeze(np.asarray(self.total_counts)), self._g(counts)).sum() 
-
-
-
-#class DSFObjective(FeatureObjective):
-#    def __init__(self, *args, **kwargs):
-#        self._order_idxs = {}
-#        self.W = None
-#        self.total_counts = None
-#        kwargs["append_ngrams"] = True
-#        super(DSFObjective, self).__init__(*args, **kwargs)
-#          
-#
-#    def get_word_features(self):
-#        self.word_features = self._ngram_vectorizer.transform(self.wordlist)
-#       
-#        if self.binarize_counts:
-#            self.word_features[self.word_features > 0 ] = 1.0
-#        self.total_counts = self.word_features.sum(axis=0)
-#        self.p = self.total_counts / float(self.word_features.sum())
-#
-#        for o in range(self.n_order, 0, -1):
-#            self._order_idxs[o] = []
-#            for k, v in self._ngram_vectorizer.vocabulary_.iteritems():
-#                if len(k) == o:
-#                    self._order_idxs[o].append(v)
-#            self._order_idxs[o] = np.asarray(self._order_idxs[o])
-#       
-#        idx_to_ngram = self._ngram_vectorizer.get_feature_names()
-#        row_idx = []
-#        col_idx = []
-#        data = []
-#        
-#        # Sort by ngram order
-#        for o in range(self.n_order, 1, -1):
-#            ograms = [idx_to_ngram[i] for i in self._order_idxs[o]]
-#            for og in ograms:
-#                o_min_1_grams = [i for i in self._order_idxs[o-1] if idx_to_ngram[i] in og]
-#                row_idx.extend(o_min_1_grams)
-#                col_idx.extend(len(o_min_1_grams) * [self._ngram_vectorizer.vocabulary_[og]])
-#                # The 3-gram weight should be a function of the 4-gram freq e.g.
-#                # This way the more common occurrence is weighted more. 
-#                data.extend(len(o_min_1_grams) * [self.total_counts[0, self._ngram_vectorizer.vocabulary_[og]]])
-#                #data.extend(len(o_min_1_grams) * [1.0]) 
-#        
-#        self.W = sparse.csr_matrix((data, (row_idx, col_idx)),
-#            shape=(self.word_features.shape[1], self.word_features.shape[1])) 
-#        
-#        
-#    
-#    def add_to_subset(self, idx):
-#        # Only add max order ngram features in current word
-#        n = min([self.n_order, len(self.wordlist[idx]) + 2])
-#        self.subset[:, self._order_idxs[n]] += self.word_features[idx, self._order_idxs[n]]
-#
-#
-#    def run(self, idx_new):
-#        vec = self.subset.copy()
-#        n = min([self.n_order, len(self.wordlist[idx_new]) + 2])
-#        vec[:, self._order_idxs[n]] += self.word_features[idx_new, self._order_idxs[n]]
-#
-#        counts = np.zeros(vec.shape)
-#        counts[:, self._order_idxs[self.n_order]] = vec[:, self._order_idxs[self.n_order]]
-#        for n in range(self.n_order, 1, -1):
-#            vec = self._g(vec)
-#            counts = self.W.dot(self._g(counts).T).T
-#            if n >= 2:
-#                counts[:, self._order_idxs[n-1]] += vec[:, self._order_idxs[n-1]]
-#            
-#        
-#        return np.multiply(np.squeeze(np.asarray(self.total_counts)), self._g(counts)).sum() 
-
-
-
-#class DSFObjective(FeatureObjective):
-#    def __init__(self, *args, **kwargs):
-#        self._order_idxs = {}
-#        self.W = None
-#        kwargs["append_ngrams"] = True
-#        super(DSFObjective, self).__init__(*args, **kwargs)
-#          
-#
-#    def get_word_features(self):
-#        self.word_features = self._ngram_vectorizer.transform(self.wordlist)
-#       
-#        for o in range(self.n_order, 0, -1):
-#            self._order_idxs[o] = []
-#            for k, v in self._ngram_vectorizer.vocabulary_.iteritems():
-#                if len(k) == o:
-#                    self._order_idxs[o].append(v)
-#            self._order_idxs[o] = np.asarray(self._order_idxs[o])
-#       
-#        idx_to_ngram = self._ngram_vectorizer.get_feature_names()
-#        row_idx = []
-#        col_idx = []
-#        data = []
-#        
-#        # Sort by ngram order
-#        for o in range(self.n_order, 1, -1):
-#            ograms = [idx_to_ngram[i] for i in self._order_idxs[o]]
-#            for og in ograms:
-#                o_min_1_grams = [i for i in self._order_idxs[o-1] if idx_to_ngram[i] in og]
-#                row_idx.extend(o_min_1_grams)
-#                col_idx.extend(len(o_min_1_grams) * [self._ngram_vectorizer.vocabulary_[og]])
-#                data.extend(len(o_min_1_grams) * [1.0]) 
-#        
-#        self.W = sparse.csr_matrix((data, (row_idx, col_idx)),
-#            shape=(self.word_features.shape[1], self.word_features.shape[1])) 
-#        
-#        self.word_features = self._ngram_vectorizer.transform(self.wordlist)
-#        if self.binarize_counts:
-#            self.word_features[self.word_features > 0 ] = 1.0
-#        self.total_counts = self.word_features.sum(axis=0)
-#        self.p = self.total_counts / float(self.word_features.sum())
-#
-#    
-#    def add_to_subset(self, idx):
-#        # Only add max order ngram features in current word
-#        n = min([self.n_order, len(self.wordlist[idx]) + 2])
-#        self.subset[:, self._order_idxs[n]] += self.word_features[idx, self._order_idxs[n]]
-#
-#
-#    def run(self, idx_new):
-#        vec = self.subset.copy()
-#        n = min([self.n_order, len(self.wordlist[idx_new]) + 2])
-#        vec[:, self._order_idxs[n]] += self.word_features[idx_new, self._order_idxs[n]]
-#
-#        counts = np.zeros(vec.shape)
-#        counts[:, self._order_idxs[self.n_order]] = vec[:, self._order_idxs[self.n_order]]
-#        for n in range(self.n_order, 1, -1):
-#            vec = self._g(vec)
-#            counts = self.W.dot(self._g(counts).T).T
-#            if n >= 2:
-#                counts[:, self._order_idxs[n-1]] += vec[:, self._order_idxs[n-1]]
-#       
-#        return self._g(counts).sum() 
-
-# I have to reimplement this. The reference is in BatchActiveLearner_Graph.py
-class FacilityLocationObjective(object):
-    def __init__(self):
-        pass
